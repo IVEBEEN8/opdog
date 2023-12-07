7355472bb9283f1c9891c2acebbe780c13fc8a66
hoho <>
2023-12-08 01:35:48 +0900

    test

--- src/main/java/com/opdoghoho/doginfo/DoginfoHC.java
+++ src/main/java/com/opdoghoho/doginfo/DoginfoHC.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/DoginfoHC")
 public class DoginfoHC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		DoginfoDAO.sido(request); // 시/도 코드 세팅 메서드
+		DoginfoDAO.sido(request); // 시/도 어트리뷰 세팅 메서드
 		
 		
 		request.getRequestDispatcher("1_adopt/1_1_info/select.jsp").forward(request, response);
