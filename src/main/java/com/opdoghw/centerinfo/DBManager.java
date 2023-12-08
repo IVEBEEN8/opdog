cdb80c50d0ae93118b49137ca1e1f16e6454f5e4
hoho <>
2023-12-08 10:13:46 +0900

    set

--- src/main/java/com/opdoghw/centerinfo/DBManager.java
+++ src/main/java/com/opdoghw/centerinfo/DBManager.java
@@ -1,7 +1,6 @@
 package com.opdoghw.centerinfo;
 
 
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -19,8 +18,8 @@ public class DBManager {
 	    // DBCP (Apache Commons Database Connection Pooling) 설정
 	    dataSource = new BasicDataSource();
 	    dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-	    
-	    
+	    // 희
+//	    dataSource.setUrl("jdbc:oracle:thin:@k9ytr1put6n556o8_high?TNS_ADMIN=/Users/heewon/Desktop/vscodeWorkSpace/Wallet_K9YTR1PUT6N556O8");
 	    dataSource.setUrl("jdbc:oracle:thin:@k9ytr1put6n556o8_high?TNS_ADMIN=/Users/namjajeonghowon/Desktop/hw/Wallet_K9YTR1PUT6N556O8");
 	    dataSource.setUsername("ADMIN");
 	    dataSource.setPassword("Thfeptmzm802!!");
