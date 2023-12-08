14b67401d491f20ac890e5fa61966e608632c08b
heewon <ivebeen888@naver.com>
2023-12-09 01:20:49 +0900

    [create][TestController] 지도 호출 테스트 컨트롤러생성

---
+++ src/main/java/com/opdoghw/centerinfo/TestController.java
@@ -0,0 +1,24 @@
+package com.opdoghw.centerinfo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestController
+ */
+@WebServlet("/TestController")
+public class TestController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("contentPage", "1_adopt/1_2_shelter/centerinfodetail1111.jsp");
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
