21a8c6720759a1e7a6c73e2c9d9c827851583bfc
heewon <ivebeen888@naver.com>
2023-12-10 14:22:36 +0900

    [create][AskAboutDogMainC] 컨트롤러생성

---
+++ src/main/java/com/opdog/askaboutdog/AskAboutDogMainC.java
@@ -0,0 +1,25 @@
+package com.opdog.askaboutdog;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/AskAboutDogMainC")
+public class AskAboutDogMainC extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setAttribute("contentPage", "4_service/4_3_askAnything/askAboutDog.jsp");
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+}
