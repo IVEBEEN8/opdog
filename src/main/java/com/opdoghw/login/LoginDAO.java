04944b2c9a17bf72259efdc58a87765a061c65fc
hoho <>
2023-12-14 17:24:41 +0900

    aerget

--- src/main/java/com/opdoghw/login/LoginDAO.java
+++ src/main/java/com/opdoghw/login/LoginDAO.java
@@ -144,16 +144,7 @@ public class LoginDAO {
 		
 	}
 	
-	public static void loginCheckMain(HttpServletRequest request) {
-		LoginDTO account = (LoginDTO)request.getSession().getAttribute("account");
-		if (account ==  null) {
-			request.setAttribute("mainLoginLogoutBtn", "main-header-loginSignup.jsp");
-		}else {
-			request.setAttribute("mainLoginLogoutBtn", "main-header-logoutMypage.jsp");
-			
-		}
-		
-	}
+	
 		
 }
 
