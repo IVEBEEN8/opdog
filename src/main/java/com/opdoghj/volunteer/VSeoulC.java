ef5f86ad6dad160b1ad0f86cd780e1be09eb155d
heewon <ivebeen888@naver.com>
2023-12-27 16:32:40 +0900

    데스데스~!

--- src/main/java/com/opdoghj/volunteer/VSeoulC.java
+++ src/main/java/com/opdoghj/volunteer/VSeoulC.java
@@ -10,18 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.opdoghw.login.LoginDAO;
 
-@WebServlet("/VtSeoulC")
-public class VtSeoulC extends HttpServlet {
+@WebServlet("/VSeoulC")
+public class VSeoulC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// 게시글 전체 조회하는 일
 		LoginDAO.LoginAccount(request);
 		LoginDAO.loginCheck(request);
 		VolunteerDAO.getAllpost(request);
-		
-		
+
 		VolunteerDAO.Paging(1, request);
-		
+
 		request.setAttribute("contentPage", "../3_volunteer/volunteerSeoul.jsp");
 		request.getRequestDispatcher("0_main/contentPage.jsp").forward(request, response);
 	}
