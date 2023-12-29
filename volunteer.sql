a46da2df4d4b384c4f5d2bf04f78c000661d191c
hyejin <6oohye@naver.com>
2023-12-29 10:52:35 +0900

    CRUD 작업

--- volunteer.sql
+++ volunteer.sql
@@ -1,6 +1,6 @@
 create table volunteer(
     v_no number(3) primary key,
-    v_title varchar2(30 char),
+    v_title varchar2(300 char),
     v_img varchar2(300char), 
     v_txt varchar2(5000char),
     v_created DATE DEFAULT TRUNC(SYSDATE),
