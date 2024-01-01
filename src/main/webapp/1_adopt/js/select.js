4c86d76b238a7bbd6a7c42046740987482c63cf9
hyejin <6oohye@naver.com>
2024-01-02 00:26:15 +0900

    css 작업

--- src/main/webapp/1_adopt/js/select.js
+++ src/main/webapp/1_adopt/js/select.js
@@ -266,7 +266,7 @@ function pagination(json){
 			$.each(data, function (index,item){
 				dataHtml += '<div class="doginfobox" id="dog">'+
 								'<div class="infoimg"> <img alt="" src="'+ item.filename+'"></div>'+
-								'<div class="doginfoWrap">'+
+							'<div class="doginfoWrap">'+
 								'<div class="infotext">'+
 									'<div class="kind"> kind: '+ item.kindCd+'</div>'+
 									'<div class="etc"> age:'+ item.age+' sex:'+ item.sexCd+' neuter:'+ item.neuterYn+'</div>'+
@@ -275,6 +275,7 @@ function pagination(json){
 									'<button id="detail"><p style="display:none;">'+JSON.stringify(item)+'</p>View More</button>'+
 									'<button id="liketrigger">like!</button>'+
 								'</div>'+'</div>'+
+
 							'</div>';
 				console.log(item)
 			});
