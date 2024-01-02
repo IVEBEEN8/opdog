c65c52eb347ac0f4afcdaac45a7e667104f77689
LEEKYEOLE <dlrufp1213@naver.com>
2024-01-02 12:06:57 +0900

    mm

---
+++ src/main/java/com/opdogkl/shop/feed/FeedSortC.java
@@ -0,0 +1,27 @@
+package com.opdogkl.shop.feed;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.opdoghw.login.LoginDAO;
+
+@WebServlet("/FeedSortC")
+public class FeedSortC extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		FeedDAO.sortFeed(request);
+		FeedDAO.paging(1, request);
+		
+		request.setAttribute("contentPage", "../2_shop/2_2_feed/feed.jsp");
+		LoginDAO.loginCheck(request);
+		request.getRequestDispatcher("0_main/contentPage.jsp").forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	
+	}
+
+}
---
+++ src/main/java/com/opdogkl/shop/snack/SnackSortC.java
@@ -0,0 +1,27 @@
+package com.opdogkl.shop.snack;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.opdoghw.login.LoginDAO;
+
+@WebServlet("/SnackSortC")
+public class SnackSortC extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		SnackDAO.sortSnack(request);
+		SnackDAO.paging(1, request);
+		
+		request.setAttribute("contentPage", "../2_shop/2_3_snack/snack.jsp");
+		LoginDAO.loginCheck(request);
+		request.getRequestDispatcher("0_main/contentPage.jsp").forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	
+	}
+
+}
